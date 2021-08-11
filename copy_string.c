6925a6d6d35c76321c7f45fea3ba35c47098cce3
h3rmit <76179581+h3rmitpurpl33@users.noreply.github.com>
2021-08-12 02:00:43 +0300

    Update copy_string.c

--- copy_string.c
+++ copy_string.c
@@ -1,1 +1,19 @@
-
+int main(){
+char str[100] , *p;
+p = str;
+int len,i=0,cnt =0;
+printf("enter text : ");
+len = read_text(str,sizeof(str),1);
+for(i=0;i<len;i++)
+{
+    if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')){
+        p[cnt] = str[i];
+        cnt++;
+    }else{
+        continue;
+    }
+}
+p[cnt] = '\0';
+printf("%s" , p);
+return 0;
+}
